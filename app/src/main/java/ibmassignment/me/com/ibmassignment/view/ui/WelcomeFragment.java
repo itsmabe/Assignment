b3274a359c78b38f664529abc61a2561d7322791
OnTrack Developer <mahmoud_benmoussa@yahoo.fr>
2018-09-20 01:59:56 +0100

    update

--- app/src/main/java/ibmassignment/me/com/ibmassignment/view/ui/WelcomeFragment.java
+++ app/src/main/java/ibmassignment/me/com/ibmassignment/view/ui/WelcomeFragment.java
@@ -2,8 +2,6 @@ package ibmassignment.me.com.ibmassignment.view.ui;
 
 
 import android.databinding.DataBindingUtil;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -13,7 +11,6 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import ibmassignment.me.com.ibm_assignment.R;
 import ibmassignment.me.com.ibm_assignment.databinding.FragmentHomeBinding;
@@ -29,14 +26,6 @@ public class WelcomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         FragmentHomeBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false);
-
-        View view = binding.getRoot();
-        ImageView bg = binding.bg;
-        Bitmap bitmap = BitmapFactory.decodeResource(getActivity().getResources(), R.drawable.blur_bg);
-//        Blurry.with(getActivity())
-//                .async()
-//                .from(bitmap)
-//                .into(bg);
 
         setHasOptionsMenu(true);
         return binding.getRoot();
